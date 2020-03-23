ebb2b448dcf79ce8574925dcd6868ac32064fe75
Fomin Dmitry <fomin.d.mail@gmail.com>
2020-03-23 11:01:12 +0300

    Django forms

--- unsafe_codes_github/searching_unsafe_codes/views.py
+++ unsafe_codes_github/searching_unsafe_codes/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 from .models import Unsafe_codes, Settings
+from .forms import SettingsForm
 
 # Create your views here.
 def main_view(request):
@@ -9,7 +11,10 @@ def main_view(request):
 
 def searching(request):
     unsafe_codes = Unsafe_codes.objects.all()
-    return render(request, 'searching_unsafe_codes/searching.html', context={'unsafe_codes': unsafe_codes, 'user_settings': {}})
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, 'searching_unsafe_codes/searching.html', context={'unsafe_codes': unsafe_codes, 'user_settings': {}})
 
 
 def searching_history(request):
@@ -17,10 +22,23 @@ def searching_history(request):
 
 
 def contacts(request):
-    settings = Settings.objects.all()[0]
-    return render(request, 'searching_unsafe_codes/contacts.html', context={'settings': settings})
+    #settings = Settings.objects.all()[0]
+    settings = Settings.objects.last()
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, 'searching_unsafe_codes/contacts.html', context={'settings': settings})
 
 
 def settings(request):
-    settings = Settings.objects.all()[0]
-    return render(request, 'searching_unsafe_codes/settings.html', context={'settings': settings})
+    #settings = Settings.objects.all()[0]
+    if request.method == 'POST':
+        form = SettingsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('searching_unsafe_codes:contacts'))
+        else:
+            return render(request, 'searching_unsafe_codes/settings.html', context={'form': form})
+    else:
+        form = SettingsForm()
+        return render(request, 'searching_unsafe_codes/settings.html', context={'form': form})#, 'settings': settings})
