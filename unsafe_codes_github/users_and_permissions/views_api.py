fe1fcf66686afa9be82c770678ba1767b970fd0b
Fomin Dmitry <fomin.d.mail@gmail.com>
2020-05-17 09:44:31 +0300

    Django REST Framework. Права, создание и обновление токена.

--- unsafe_codes_github/users_and_permissions/views_api.py
+++ unsafe_codes_github/users_and_permissions/views_api.py
@@ -1,9 +1,11 @@
 from .models import AdvancedUser
 from .serializers import AdvancedUserSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
 
 class AdvancedUserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUser]
     queryset = AdvancedUser.objects.all()
     serializer_class = AdvancedUserSerializer
 
